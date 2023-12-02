0c7922a0f387603589b4e71d4d62831bc53e132b
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-01 21:27:27 -0800

    solution for day 2

--- src/day-02/index.js
+++ src/day-02/index.js
@@ -1,16 +1,85 @@
-const { getInput } = require('../utils')
+const { getInput, sum } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .reduce((acc, line) => {
+      const [gameStr, pullsStr] = line.split(': ')
+      const id = gameStr.replace('Game ', '')
+      const pulls = pullsStr.split('; ')
+
+      const results = pulls.map(pull => {
+        const cubes = pull.split(', ')
+
+        const result = {
+          blue: 0,
+          green: 0,
+          red: 0,
+        }
+
+        for (const cube of cubes) {
+          const [int, color] = cube.split(' ')
+          result[color] = parseInt(int, 10)
+        }
+
+        return result
+      })
+
+      acc[id] = results
+      return acc
+    }, {})
+
+const BAG = {
+  blue: 14,
+  green: 13,
+  red: 12,
+}
+
+const isPullPossible = ({ blue, green, red }) =>
+  blue <= BAG.blue && green <= BAG.green && red <= BAG.red
+
+const isGamePossible = pulls => pulls.every(isPullPossible)
 
 function solution1(input) {
   const formatted = formatInput(input)
+  const possibleGames = Object.entries(formatted).filter(([_key, pulls]) =>
+    isGamePossible(pulls)
+  )
+  const nums = possibleGames.map(([key]) => parseInt(key, 10))
+
+  return sum(nums)
 }
 
-function solution2(input) {}
+const getMinimumRequired = pulls => {
+  const result = { blue: 0, green: 0, red: 0 }
+
+  for (const pull of pulls) {
+    if (pull.blue > result.blue) result.blue = pull.blue
+    if (pull.green > result.green) result.green = pull.green
+    if (pull.red > result.red) result.red = pull.red
+  }
+
+  return result
+}
+
+const getPower = ({ blue, green, red }) => blue * green * red
+
+function solution2(input) {
+  const formatted = formatInput(input)
+  const minimums = Object.values(formatted).map(getMinimumRequired)
+  const powers = minimums.map(getPower)
+
+  return sum(powers)
+}
+
+// console.log(solution1(data)) // 2317
+
+// console.log(solution2(data)) // 74804
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-02/index.test.js
+++ src/day-02/index.test.js
@@ -1,11 +1,17 @@
 const { solution1, solution2 } = require('.')
 
-const input = `input data for day 02`
+const input = `
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(8)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(2286)
 })
