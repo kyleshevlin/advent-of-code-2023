b5f7dc301837852ab86bc94dd41e92a95a9e5b5b
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-04 09:06:22 -0800

    tidy up day 4

--- src/day-04/index.js
+++ src/day-04/index.js
@@ -6,7 +6,7 @@ const formatInput = input =>
   input
     .trim()
     .split('\n')
-    .map(line => {
+    .map((line, idx) => {
       const [cardNumber, numbers] = line.split(': ')
       const [winnersStr, yoursStr] = numbers.split(' | ')
 
@@ -19,6 +19,8 @@ const formatInput = input =>
 
 const findMatches = ({ winners, yours }) => intersection(winners, yours)
 
+// It might suprise people that any number to the power of 0 is 1 (n ** 0 === 1)
+// Hence, why I use a ternary to return 0 in the case of no matches
 const getWorth = set => (set.size ? 2 ** (set.size - 1) : 0)
 
 function solution1(input) {
@@ -30,44 +32,41 @@ function solution1(input) {
 
 function solution2(input) {
   const cards = formatInput(input)
+  const copyQuantitiesById = {}
 
-  const copiesMap = {}
-
-  // Keeping this in scope so I don't have to pass `cards` or `copiesMap` into it
+  // Keeping this in scope so I don't have to pass `cards`
+  // or `copyQuantitiesById` into it
   function processCard(card, cardIdx) {
     const matches = findMatches(card)
 
     if (!matches.size) return
 
-    Array(matches.size)
-      .fill()
-      .forEach((_, idx) => {
-        const nextIdx = cardIdx + 1 + idx
-        const id = cards[nextIdx].id
+    let i = 0
+    for (i; i < matches.size; i++) {
+      const nextIdx = cardIdx + 1 + i
+      const id = cards[nextIdx].id
 
-        if (!copiesMap[id]) {
-          copiesMap[id] = 0
-        }
-
-        copiesMap[id]++
-      })
-  }
-
-  let idx = 0
-  for (idx; idx < cards.length; idx++) {
-    const original = cards[idx]
-    processCard(original, idx)
-
-    const copies = copiesMap[original.id]
-
-    if (copies) {
-      for (let i = 0; i < copies; i++) {
-        processCard(original, idx)
+      if (!copyQuantitiesById[id]) {
+        copyQuantitiesById[id] = 0
       }
+
+      copyQuantitiesById[id]++
     }
   }
 
-  return cards.length + sum(Object.values(copiesMap))
+  let cardIdx = 0
+  for (cardIdx; cardIdx < cards.length; cardIdx++) {
+    const original = cards[cardIdx]
+    const copies = copyQuantitiesById[original.id] ?? 0
+    // original card === 1
+    const total = 1 + copies
+
+    for (let i = 0; i < total; i++) {
+      processCard(original, cardIdx)
+    }
+  }
+
+  return cards.length + sum(Object.values(copyQuantitiesById))
 }
 
 // console.log(solution1(data)) // 20107
