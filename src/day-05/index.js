be1f3be0dcfbdd4b49185884ee1fdefc4ced73b8
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-04 21:58:30 -0800

    day 5 solution 1 and passing wip of solution 2

--- src/day-05/index.js
+++ src/day-05/index.js
@@ -2,15 +2,196 @@ const { getInput } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input => {
+  const [
+    seeds,
+    seedToSoil,
+    soilToFertilizer,
+    fertilizerToWater,
+    waterToLight,
+    lightToTemp,
+    tempToHumidity,
+    humidtyToLocation,
+  ] = input.trim().split('\n\n')
+
+  const result = {}
+
+  result.seeds = seeds.split(': ')[1].split(' ').map(Number)
+
+  result.seedToSoilMap = parseSection(seedToSoil, 'seed-to-soil map:')
+  result.soilToFertilizerMap = parseSection(
+    soilToFertilizer,
+    'soil-to-fertlizer map:'
+  )
+  result.fertilizerToWaterMap = parseSection(
+    fertilizerToWater,
+    'fertilizer-to-water map:'
+  )
+  result.waterToLightMap = parseSection(waterToLight, 'water-to-light map:')
+  result.lightToTempMap = parseSection(lightToTemp, 'light-to-temperature map:')
+  result.tempToHumidityMap = parseSection(
+    tempToHumidity,
+    'temp-to-humidity map:'
+  )
+  result.humidtyToLocationMap = parseSection(
+    humidtyToLocation,
+    'humidity-to-location map:'
+  )
+
+  return result
+}
+
+const parseSection = (section, heading) => {
+  const ranges = section.replace(heading, '').trim().split('\n').map(parseRange)
+
+  return {
+    ranges,
+    getNextKey(key) {
+      for (const range of ranges) {
+        const nextKey = range.getNextKey(key)
+
+        if (nextKey === key) continue
+
+        return nextKey
+      }
+
+      return key
+    },
+  }
+}
+
+const parseRange = line => {
+  const [destStart, srcStart, length] = line.split(' ').map(Number)
+
+  // would be good to have methods that could check a range if a value should be found in it
+
+  return {
+    destStart,
+    srcStart,
+    length,
+    getNextKey(key) {
+      if (key >= srcStart && key <= srcStart + length - 1) {
+        return destStart + (key - srcStart)
+      }
+
+      return key
+    },
+  }
+}
 
 function solution1(input) {
   const formatted = formatInput(input)
+
+  const {
+    seeds,
+    seedToSoilMap,
+    soilToFertilizerMap,
+    fertilizerToWaterMap,
+    waterToLightMap,
+    lightToTempMap,
+    tempToHumidityMap,
+    humidtyToLocationMap,
+  } = formatted
+
+  const order = [
+    seedToSoilMap,
+    soilToFertilizerMap,
+    fertilizerToWaterMap,
+    waterToLightMap,
+    lightToTempMap,
+    tempToHumidityMap,
+    humidtyToLocationMap,
+  ]
+
+  let lowest = Infinity
+  for (const seed of seeds) {
+    let result = seed
+
+    for (const map of order) {
+      const nextKey = map.getNextKey(result)
+      result = nextKey
+    }
+
+    if (result < lowest) {
+      lowest = result
+    }
+  }
+
+  // if a seed number is not found in a map, then the number of the corresponding
+  // value is the same. So if we have seed 0 and the seed-to-soil map does not have
+  // a key of 0, then its soil number is _also_ 0.
+
+  return lowest
 }
 
-function solution2(input) {}
+function solution2(input) {
+  const formatted = formatInput(input)
+
+  // adjust the seeds
+  formatted.seeds = makeSeedRanges(formatted.seeds)
+
+  const {
+    seeds,
+    seedToSoilMap,
+    soilToFertilizerMap,
+    fertilizerToWaterMap,
+    waterToLightMap,
+    lightToTempMap,
+    tempToHumidityMap,
+    humidtyToLocationMap,
+  } = formatted
+
+  const order = [
+    seedToSoilMap,
+    soilToFertilizerMap,
+    fertilizerToWaterMap,
+    waterToLightMap,
+    lightToTempMap,
+    tempToHumidityMap,
+    humidtyToLocationMap,
+  ]
+
+  let lowest = Infinity
+  for (const seed of seeds) {
+    // LOL, this won't work for the actual data
+    for (let i = seed.start; i < seed.start + seed.length; i++) {
+      let result = i
+
+      for (const map of order) {
+        const nextKey = map.getNextKey(result)
+        result = nextKey
+      }
+
+      if (result < lowest) {
+        lowest = result
+      }
+    }
+  }
+
+  return lowest
+}
+
+function makeSeedRanges(seeds) {
+  const clone = [...seeds]
+  const result = []
+
+  while (clone.length) {
+    const start = clone.shift()
+    const length = clone.shift()
+
+    result.push({ start, length })
+  }
+
+  return result
+}
+
+// console.log(solution1(data)) // 331445006
+
+// console.log(solution2(data))
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
+  parseRange,
+  parseSection,
 }
--- src/day-05/index.test.js
+++ src/day-05/index.test.js
@@ -1,11 +1,74 @@
-const { solution1, solution2 } = require('.')
+const { solution1, solution2, parseRange, parseSection } = require('.')
 
-const input = `input data for day 05`
+const input = `
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(35)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(46)
+})
+
+test('parseRange', () => {
+  const range = parseRange('50 98 2')
+
+  expect(range.destStart).toEqual(50)
+  expect(range.srcStart).toEqual(98)
+  expect(range.length).toEqual(2)
+  expect(range.getNextKey(97)).toEqual(97)
+  expect(range.getNextKey(98)).toEqual(50)
+  expect(range.getNextKey(99)).toEqual(51)
+  expect(range.getNextKey(100)).toEqual(100)
+})
+
+test('parseSection', () => {
+  const section = parseSection(
+    `
+seed-to-soil map:
+50 98 2
+52 50 48
+`,
+    'seed-to-soil map:'
+  )
+
+  expect(section.ranges.length).toEqual(2)
+  expect(section.getNextKey(49)).toEqual(49)
+  expect(section.getNextKey(50)).toEqual(52)
+  expect(section.getNextKey(53)).toEqual(55)
+  expect(section.getNextKey(100)).toEqual(100)
 })
