06ae65cd8f1841b07c8adbf28082fadd9044b7ce
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-08 22:43:50 -0800

    day 9 solutions

--- src/day-09/index.js
+++ src/day-09/index.js
@@ -1,16 +1,71 @@
-const { getInput } = require('../utils')
+const { getInput, sum } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input => input.trim().split('\n')
 
-function solution1(input) {
-  const formatted = formatInput(input)
+const createHistory = str => {
+  const sequence = str.split(' ').map(Number)
+
+  function getSequences() {
+    const result = [sequence]
+
+    let currentSequence = sequence
+    while (!currentSequence.every(n => n === 0)) {
+      const clone = [...currentSequence]
+      const nextSequence = []
+      let currentNum = clone.shift()
+
+      while (clone.length) {
+        const nextNum = clone.shift()
+        const diff = nextNum - currentNum
+        nextSequence.push(diff)
+        currentNum = nextNum
+      }
+
+      result.push(nextSequence)
+      currentSequence = nextSequence
+    }
+
+    return result
+  }
+
+  function getNextRightValue() {
+    return getSequences()
+      .map(s => s.at(-1))
+      .reduceRight((acc, cur) => acc + cur)
+  }
+
+  function getNextLeftValue() {
+    return getSequences()
+      .map(s => s[0])
+      .reduceRight((acc, cur) => cur - acc)
+  }
+
+  return { sequence, getNextRightValue, getNextLeftValue }
 }
 
-function solution2(input) {}
+function solution1(input) {
+  const lines = formatInput(input)
+  const histories = lines.map(createHistory)
+  const nextValues = histories.map(h => h.getNextRightValue())
+
+  return sum(nextValues)
+}
+
+// console.log(solution1(data)) // 1666172641
+
+function solution2(input) {
+  const lines = formatInput(input)
+  const histories = lines.map(createHistory)
+  const nextValues = histories.map(h => h.getNextLeftValue())
+
+  return sum(nextValues)
+}
+
+// console.log(solution2(data)) // 933
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-09/index.test.js
+++ src/day-09/index.test.js
@@ -1,11 +1,15 @@
 const { solution1, solution2 } = require('.')
 
-const input = `input data for day 09`
+const input = `
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(114)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(2)
 })
