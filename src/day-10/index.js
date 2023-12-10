353c7b1a5a46990d66a31cb0799e772b15a58440
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-10 11:26:14 -0800

    solutions for day 10

--- src/day-10/index.js
+++ src/day-10/index.js
@@ -1,16 +1,240 @@
-const { getInput } = require('../utils')
+const { getInput, createStack } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .map(line => line.trim())
 
-function solution1(input) {
-  const formatted = formatInput(input)
+function findStart(grid) {
+  for (const [rowIdx, row] of grid.entries()) {
+    const chars = row.split('')
+    const colIdx = chars.findIndex(c => c === 'S')
+
+    if (colIdx > -1) return [rowIdx, colIdx]
+  }
 }
 
-function solution2(input) {}
+// Based on the direction you went into the pipe, what direction
+// would you be going next.
+const PIPES_TO_CURRENT_DIRECTION_TO_NEXT_DIRECTION = {
+  '|': { S: 'S', N: 'N' },
+  '-': { E: 'E', W: 'W' },
+  L: { S: 'E', W: 'N' },
+  J: { S: 'W', E: 'N' },
+  7: { N: 'W', E: 'S' },
+  F: { N: 'E', W: 'S' },
+}
+
+function getStartingDirs(grid, r, c) {
+  const startingDirs = []
+
+  if (['|', '7', 'F'].includes(grid?.[r - 1]?.[c])) {
+    startingDirs.push('N')
+  }
+
+  if (['-', 'L', 'F'].includes(grid?.[r]?.[c - 1])) {
+    startingDirs.push('W')
+  }
+
+  if (['-', 'J', '7'].includes(grid?.[r]?.[c + 1])) {
+    startingDirs.push('E')
+  }
+
+  if (['|', 'L', 'J'].includes(grid?.[r + 1]?.[c])) {
+    startingDirs.push('S')
+  }
+
+  return startingDirs
+}
+
+function getNextLocation(grid, dir, location) {
+  const [row, col] = location
+
+  switch (dir) {
+    case 'N':
+      return [row - 1, col]
+    case 'E':
+      return [row, col + 1]
+    case 'S':
+      return [row + 1, col]
+    case 'W':
+      return [row, col - 1]
+  }
+}
+
+function getNextStep(grid, walker) {
+  const { dir, location } = walker
+
+  const nextLocation = getNextLocation(grid, dir, location)
+  const [nextRow, nextCol] = nextLocation
+  const nextSquare = grid?.[nextRow]?.[nextCol]
+  const nextDir = PIPES_TO_CURRENT_DIRECTION_TO_NEXT_DIRECTION[nextSquare][dir]
+
+  return { dir: nextDir, location: nextLocation }
+}
+
+function areSameLocations(loc1, loc2) {
+  const [r1, c1] = loc1
+  const [r2, c2] = loc2
+
+  return r1 === r2 && c1 === c2
+}
+
+function solution1(input) {
+  const grid = formatInput(input)
+  const start = findStart(grid)
+  const startingDirs = getStartingDirs(grid, ...start)
+
+  let steps = 0
+  let walkers = startingDirs.map(dir => ({
+    dir,
+    location: start,
+  }))
+
+  do {
+    const nextWalkers = walkers.map(w => getNextStep(grid, w))
+    steps++
+    walkers = nextWalkers
+  } while (!areSameLocations(walkers[0].location, walkers[1].location))
+
+  return steps
+}
+
+// console.log(solution1(data)) // 7086
+
+function closureFill(grid, startingRow, startingCol) {
+  const clone = grid.map(x => x.slice()).slice()
+
+  const isValid = (a, b) => {
+    const char = clone?.[a]?.[b]
+    return char && char !== 'X' && char !== 'O'
+  }
+
+  const DIRS = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ]
+
+  // stack based flood fill (recursive caused stack overflow errors)
+  function fill(row, col) {
+    const stack = createStack()
+    stack.push([row, col])
+
+    while (!stack.isEmpty()) {
+      const [currentRow, currentCol] = stack.pop()
+
+      for (const [rowAdjustment, colAdjustment] of DIRS) {
+        const nextRow = currentRow + rowAdjustment
+        const nextCol = currentCol + colAdjustment
+
+        if (isValid(nextRow, nextCol)) {
+          clone[nextRow][nextCol] = 'O'
+          stack.push([nextRow, nextCol])
+        }
+      }
+    }
+  }
+
+  fill(startingRow, startingCol)
+
+  return clone
+}
+
+function solution2(input) {
+  const grid = formatInput(input)
+
+  /**
+   * I realized that we could extend the grid, both vertically and horizontally
+   * to ensure that no 2 pipes in the loop are adjacent, and therefore disrupting
+   * the ability to flood fill.
+   *
+   * We'll add the extensions and a perimeter to the grid, and then once we've
+   * flooded the grid, we'll remove them in order to get the correct count.
+   */
+  const withExtensions = grid
+    .map(row => row.split('').reduce((acc, cur) => [...acc, cur, '-'], []))
+    .reduce((acc, cur) => [...acc, cur, Array(cur.length).fill('|')], [])
+    .map(row => row.join(''))
+
+  const originalStart = findStart(grid)
+  const extendedStart = findStart(withExtensions)
+  const startingDirs = getStartingDirs(grid, ...originalStart)
+
+  let walkers = startingDirs.map(dir => ({
+    dir,
+    location: extendedStart,
+  }))
+
+  do {
+    const nextWalkers = walkers.map(w => getNextStep(withExtensions, w))
+
+    // Update the grid where the walkers have been to an 'X'
+    walkers.forEach(walker => {
+      const [r, c] = walker.location
+      const chars = withExtensions[r].split('')
+      chars[c] = 'X'
+      withExtensions[r] = chars.join('')
+    })
+
+    walkers = nextWalkers
+  } while (!areSameLocations(walkers[0].location, walkers[1].location))
+
+  /**
+   * The pipe loop isn't getting closed because we don't have a way in the
+   * do..while loop to put an 'X' on the final spot, so we're going to do that
+   * manually here
+   */
+  const [r, c] = walkers[0].location
+  const chars = withExtensions[r].split('')
+  chars[c] = 'X'
+  withExtensions[r] = chars.join('')
+
+  // Now we're going to prepare the grid to flood fill.
+  // Set every non 'X' to 'I'
+  const withIs = withExtensions.map(line =>
+    line.split('').map(c => (c !== 'X' ? 'I' : 'X'))
+  )
+
+  /**
+   * Add a perimeter of I's so we can flood fill and ensure to get everything
+   * If we don't, then it's possible for there to be outside values that aren't
+   * accessible because the flooding won't go into the undefined area of the grid
+   */
+  const withPerimeter = [
+    Array(withIs.at(0).length + 2).fill('I'),
+    ...withIs.map(row => ['I', ...row, 'I']),
+    Array(withIs.at(0).length + 2).fill('I'),
+  ]
+
+  const floodedGrid = closureFill(withPerimeter, 0, 0)
+
+  /**
+   * Now, we're going to remove all of the extra cells we added, starting with
+   * the perimeter
+   */
+  const withoutPerimeter = floodedGrid
+    .filter((_, idx) => idx !== 0 && idx !== floodedGrid.length - 1)
+    .map(r => {
+      return r.filter((_, i) => i !== 0 && i !== r.length - 1)
+    })
+
+  // And now any of our extensions
+  const withoutExtensions = withoutPerimeter
+    .filter((_, i) => i % 2 === 0)
+    .map(r => r.filter((_, i) => i % 2 === 0))
+
+  // We're left with a grid of our original size and only the correct 'I's remaining
+  return withoutExtensions.flat().filter(c => c === 'I').length
+}
+
+// console.log(solution2(data)) // 317
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-10/index.test.js
+++ src/day-10/index.test.js
@@ -1,11 +1,75 @@
 const { solution1, solution2 } = require('./')
 
-const input = `input data for day 10`
+const input1 = `
+.....
+.S-7.
+.|.|.
+.L-J.
+.....
+`
+
+const input2 = `
+-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF
+`
+
+const input3 = `
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input1)).toEqual(4)
+  expect(solution1(input2)).toEqual(4)
+  expect(solution1(input3)).toEqual(8)
 })
 
+const input4 = `
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........
+`
+
+const input5 = `
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+`
+
+const input6 = `
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+`
+
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input4)).toEqual(4)
+  expect(solution2(input5)).toEqual(8)
+  expect(solution2(input6)).toEqual(10)
 })
