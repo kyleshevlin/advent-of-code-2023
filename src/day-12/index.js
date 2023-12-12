a4e5ded7f6fe215e934f636a7ee860ec4e3d0213
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-11 21:51:42 -0800

    day 12 pt 1 complete pt 2 wip

--- src/day-12/index.js
+++ src/day-12/index.js
@@ -1,16 +1,93 @@
-const { getInput } = require('../utils')
+const { getInput, sum } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .map(row => {
+      const [springs, groupingsStr] = row.split(' ')
+      const groupings = groupingsStr.split(',').map(Number)
 
-function solution1(input) {
-  const formatted = formatInput(input)
+      return [springs, groupings]
+    })
+
+function getCombos(length, current = '') {
+  if (length === 0) return [current]
+
+  return [
+    ...getCombos(length - 1, current + '.'),
+    ...getCombos(length - 1, current + '#'),
+  ]
 }
 
-function solution2(input) {}
+function isValid(spring, groupings) {
+  const groups = spring.replaceAll(/\.+/g, ' ').split(' ').filter(Boolean)
+
+  if (groups.length !== groupings.length) return false
+
+  return groupings.every((num, idx) => num === groups[idx]?.length)
+}
+
+function findNumberOfPossibleArrangements(springs, groupings) {
+  // Make every possible permutation given the springs
+  // Find out how many question marks you have
+  const unknowns = springs.split('').filter(c => c === '?').length
+
+  // Make every possible combo of operational/broken you can for that many unknowns
+  const combos = getCombos(unknowns)
+
+  // Loop through the combos, replacing all the ?s with the chars of it
+  // Check if it's valid. If so, add it to the count
+
+  let result = 0
+
+  for (const combo of combos) {
+    const comboChars = combo.split('')
+    const spring = springs
+      .split('')
+      .map(c => (c === '?' ? comboChars.shift() : c))
+      .join('')
+
+    if (isValid(spring, groupings)) {
+      result++
+    }
+  }
+
+  return result
+}
+
+function solution1(input) {
+  const rows = formatInput(input)
+  const arrangements = rows.map(row => findNumberOfPossibleArrangements(...row))
+
+  return sum(arrangements)
+}
+
+// console.log(solution1(data)) // 7857
+
+function solution2(input) {
+  const rows = formatInput(input)
+  const unfoldedRows = rows.map(row => {
+    const [springs, groupings] = row
+
+    return [
+      Array(5).fill(springs).join('?'),
+      Array(5)
+        .fill(undefined)
+        .flatMap(() => [...groupings]),
+    ]
+  })
+
+  const arrangements = unfoldedRows.map(row =>
+    findNumberOfPossibleArrangements(...row)
+  )
+
+  return sum(arrangements)
+}
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-12/index.test.js
+++ src/day-12/index.test.js
@@ -1,11 +1,30 @@
 const { solution1, solution2 } = require('./')
 
-const input = `input data for day 12`
+const input = `
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1('???.### 1,1,3')).toEqual(1)
+  expect(solution1('.??..??...?##. 1,1,3')).toEqual(4)
+  expect(solution1('?#?#?#?#?#?#?#? 1,3,1,6')).toEqual(1)
+  expect(solution1('????.#...#... 4,1,1')).toEqual(1)
+  expect(solution1('????.######..#####. 1,6,5')).toEqual(4)
+  expect(solution1('?###???????? 3,2,1')).toEqual(10)
+  expect(solution1(input)).toEqual(21)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2('???.### 1,1,3')).toEqual(1)
+  // expect(solution2('.??..??...?##. 1,1,3')).toEqual(16384)
+  // expect(solution2('?#?#?#?#?#?#?#? 1,3,1,6')).toEqual(1)
+  // expect(solution2('????.#...#... 4,1,1')).toEqual(16)
+  // expect(solution2('????.######..#####. 1,6,5')).toEqual(2500)
+  // expect(solution2('?###???????? 3,2,1')).toEqual(506250)
+  // expect(solution2(input)).toEqual(21)
 })
