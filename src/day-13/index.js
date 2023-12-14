4ca5c5096ec73a62174182ef2765742c705fa725
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-13 23:19:27 -0800

    day 13 pt 1

--- src/day-13/index.js
+++ src/day-13/index.js
@@ -2,15 +2,72 @@ const { getInput } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input => input.trim().split('\n\n')
+
+function findMirror(note) {
+  const rows = note.split('\n')
+  const rowResult = findReflection(rows, 'row')
+
+  if (rowResult) return rowResult
+
+  const cols = []
+  rows.forEach(row => {
+    row.split('').forEach((char, idx) => {
+      if (!cols[idx]) cols[idx] = []
+      cols[idx].push(char)
+    })
+  })
+
+  const colResult = findReflection(
+    cols.map(c => c.join('')),
+    'column'
+  )
+
+  return colResult
+}
+
+function findReflection(rows, type) {
+  let i = 0
+  for (i; i < rows.length; i++) {
+    const rowsBefore = rows.slice(0, i + 1)
+    const rowsAfter = rows.slice(i + 1)
+    const rowsToCheck = Math.min(rowsBefore.length, rowsAfter.length)
+    const rowsBeforeAdj = rowsBefore.toReversed().slice(0, rowsToCheck)
+    const rowsAfterAdj = rowsAfter.slice(0, rowsToCheck)
+
+    if (!rowsBeforeAdj.length || !rowsAfterAdj.length) continue
+
+    let j = 0
+    let success = true
+    for (j; j < rowsToCheck; j++) {
+      if (rowsBeforeAdj[j] !== rowsAfterAdj[j]) {
+        success = false
+        break
+      }
+    }
+
+    if (success) {
+      return { type, idx: i + 1 }
+    }
+  }
+}
 
 function solution1(input) {
-  const formatted = formatInput(input)
+  const notes = formatInput(input)
+  const mirrors = notes.map(findMirror)
+  const values = mirrors.reduce((acc, cur) => {
+    const value = cur.type === 'row' ? cur.idx * 100 : cur.idx
+    return acc + value
+  }, 0)
+
+  return values
 }
+
+// console.log(solution1(data)) // 37113
 
 function solution2(input) {}
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-13/index.test.js
+++ src/day-13/index.test.js
@@ -1,11 +1,29 @@
 const { solution1, solution2 } = require('./')
 
-const input = `input data for day 13`
+const input = `
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input.split('\n\n').at(0))).toEqual(5)
+  expect(solution1(input.split('\n\n').at(1))).toEqual(400)
+  expect(solution1(input)).toEqual(405)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(400)
 })
