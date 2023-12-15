e8429ec4d72abf5cebbf2a7ab7043d53b6e5ffe7
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-14 21:12:26 -0800

    day 15 pt 1

--- src/day-15/index.js
+++ src/day-15/index.js
@@ -1,16 +1,33 @@
-const { getInput } = require('../utils')
+const { getInput, sum } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input => input.trim().split(',')
+
+function hash(str) {
+  let value = 0
+
+  for (const char of str.split('')) {
+    value += char.charCodeAt(0)
+    value *= 17
+    value %= 256
+  }
+
+  return value
+}
 
 function solution1(input) {
-  const formatted = formatInput(input)
+  const instructions = formatInput(input)
+
+  return sum(instructions.map(hash))
 }
+
+// console.log(solution1(data)) // 514025
 
 function solution2(input) {}
 
 module.exports = {
+  hash,
   solution1,
-  solution2
+  solution2,
 }
--- src/day-15/index.test.js
+++ src/day-15/index.test.js
@@ -1,9 +1,13 @@
-const { solution1, solution2 } = require('./')
+const { solution1, solution2, hash } = require('./')
 
-const input = `input data for day 15`
+test('hash', () => {
+  expect(hash('HASH')).toEqual(52)
+})
+
+const input = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(1320)
 })
 
 test('solution2', () => {
