b4a2b99a07648ca0b0ea4b02ad235cc3f6535997
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-14 21:39:21 -0800

    day 15 pt 2

--- src/day-15/index.js
+++ src/day-15/index.js
@@ -24,7 +24,48 @@ function solution1(input) {
 
 // console.log(solution1(data)) // 514025
 
-function solution2(input) {}
+function solution2(input) {
+  const instructions = formatInput(input)
+
+  const boxes = {}
+
+  for (const instruction of instructions) {
+    const label = instruction.split(/=|-/).at(0)
+    const key = hash(label)
+
+    if (!boxes[key]) boxes[key] = []
+
+    if (instruction.includes('-')) {
+      boxes[key] = boxes[key].filter(box => box.label !== label)
+    }
+
+    if (instruction.includes('=')) {
+      const idx = boxes[key].findIndex(box => box.label === label)
+      const focalLength = instruction.split('=').at(1)
+      const nextBox = { label, focalLength: Number(focalLength) }
+
+      if (idx === -1) {
+        boxes[key].push(nextBox)
+        continue
+      }
+
+      boxes[key][idx] = nextBox
+    }
+  }
+
+  const powers = []
+
+  for (const [boxNumber, lenses] of Object.entries(boxes)) {
+    for (const [idx, { focalLength }] of lenses.entries()) {
+      const value = (1 + Number(boxNumber)) * (idx + 1) * focalLength
+      powers.push(value)
+    }
+  }
+
+  return sum(powers)
+}
+
+// console.log(solution2(data)) // 244461
 
 module.exports = {
   hash,
--- src/day-15/index.test.js
+++ src/day-15/index.test.js
@@ -11,5 +11,5 @@ test('solution1', () => {
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(145)
 })
