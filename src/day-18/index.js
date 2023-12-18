990350729601138cc1dd4d8f8ed48d98d7b71066
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-17 22:11:48 -0800

    day 18 pt 1 complete

--- src/day-18/index.js
+++ src/day-18/index.js
@@ -1,16 +1,166 @@
-const { getInput } = require('../utils')
+const { getInput, createStack } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .map(line => {
+      const [dir, amount, wrappedColor] = line.split(' ')
+      const color = wrappedColor.replace('(', '').replace(')', '')
+
+      return { dir, amount: Number(amount), color }
+    })
+
+function getNextCoord(dir, row, col) {
+  switch (dir) {
+    case 'U':
+      return [row - 1, col]
+    case 'D':
+      return [row + 1, col]
+    case 'R':
+      return [row, col + 1]
+    case 'L':
+      return [row, col - 1]
+  }
+}
+
+function dig(instructions) {
+  const visited = {
+    '0,0': true,
+  }
+
+  let current = [0, 0]
+  for (const inst of instructions) {
+    const { dir, amount } = inst
+
+    for (let i = 0; i < amount; i++) {
+      const nextCoord = getNextCoord(dir, ...current)
+      visited[nextCoord.join(',')] = true
+      current = nextCoord
+    }
+  }
+
+  return visited
+}
+
+/**
+ * We're going to make the top-most left-most visited spot 0,0 and adjust all
+ * the keys accordingly
+ */
+function normalizeVisited(visited) {
+  const keys = Object.keys(visited)
+  const rows = keys.map(key => key.split(',').map(Number).at(0))
+  const cols = keys.map(key => key.split(',').map(Number).at(1))
+
+  const minRow = Math.min(...rows)
+  const minCol = Math.min(...cols)
+
+  const rowAdj = minRow * -1
+  const colAdj = minCol * -1
+
+  const normalizedKeys = keys.map(key => {
+    const [row, col] = key.split(',').map(Number)
+
+    return [row + rowAdj, col + colAdj].join(',')
+  })
+
+  return normalizedKeys.reduce((acc, key) => {
+    acc[key] = true
+    return acc
+  }, {})
+}
+
+function getPerimeter(visited) {
+  const keys = Object.keys(visited)
+  const rows = keys.map(key => key.split(',').map(Number).at(0))
+  const cols = keys.map(key => key.split(',').map(Number).at(1))
+
+  const minRow = Math.min(...rows)
+  const maxRow = Math.max(...rows)
+  const minCol = Math.min(...cols)
+  const maxCol = Math.max(...cols)
+
+  const rowDiff = maxRow + 1 - minRow
+  const colDiff = maxCol + 1 - minCol
+
+  // We're probably gonna have to be able to shift the whole map so the top most
+  // left most ends up 0,0
+
+  const grid = Array(rowDiff)
+    .fill()
+    .map(() => Array(colDiff).fill('.'))
+
+  for (const key of keys) {
+    const [r, c] = key.split(',')
+    grid[r][c] = '#'
+  }
+
+  return grid
+}
+
+function fillGrid(grid, startRow, startCol) {
+  const clone = grid.map(x => x.slice()).slice()
+
+  const isValid = (r, c) => {
+    const char = clone?.[r]?.[c]
+    return char && char !== '#'
+  }
+
+  const DIRS = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ]
+
+  function fill(row, col) {
+    const stack = createStack()
+    stack.push([row, col])
+
+    while (!stack.isEmpty()) {
+      const [r, c] = stack.pop()
+
+      for (const [rAdj, cAdj] of DIRS) {
+        const nr = r + rAdj
+        const nc = c + cAdj
+
+        if (isValid(nr, nc)) {
+          clone[nr][nc] = '#'
+          stack.push([nr, nc])
+        }
+      }
+    }
+  }
+
+  // TODO: This assumption doesn't work
+  fill(startRow, startCol)
+
+  return clone
+}
+
+function visualize(visited) {
+  const grid = getPerimeter(visited)
+  console.log(grid.map(row => row.join('')).join('\n'))
+}
 
 function solution1(input) {
-  const formatted = formatInput(input)
+  const instructions = formatInput(input)
+  const visited = dig(instructions)
+  const normalized = normalizeVisited(visited)
+  const perimeter = getPerimeter(normalized)
+  const firstRowIdx = perimeter.findIndex(row => row.at(0) === '#')
+  const filled = fillGrid(perimeter, firstRowIdx + 1, 1)
+
+  return filled.flatMap(row => row.filter(c => c === '#')).length
 }
+
+console.log(solution1(data))
 
 function solution2(input) {}
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-18/index.test.js
+++ src/day-18/index.test.js
@@ -1,9 +1,29 @@
 const { solution1, solution2 } = require('./')
 
-const input = `input data for day 18`
+const input = `
+R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(62)
+
+  const lines = input.trim().split('\n')
+  const startElsewhere = [...lines.slice(10), ...lines.slice(0, 10)].join('\n')
+
+  expect(solution1(startElsewhere)).toEqual(62)
 })
 
 test('solution2', () => {
