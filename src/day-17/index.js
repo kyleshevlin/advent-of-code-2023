2db7d5a3a4a8ca50962e90f3a860f998db8a7ea6
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-17 20:59:34 -0800

    day 17 pt 1 wip

--- src/day-17/index.js
+++ src/day-17/index.js
@@ -2,15 +2,87 @@ const { getInput } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input => input.trim().split('\n')
+
+function createGraph() {
+  const nodes = []
+  const edges = {}
+
+  return {
+    nodes: () => nodes,
+    edges: () => edges,
+    addNode: node => {
+      nodes.push(node)
+      edges[node] = []
+    },
+    addEdge: (from, to, weight, dir) => {
+      edges[from].push({ to, weight, dir })
+    },
+    inspect: () => {
+      console.log(JSON.stringify({ nodes, edges }))
+    },
+  }
+}
+
+function traverse(graph, startNode) {
+  const distances = {}
+  const visited = {}
+  const path = {}
+  const queue = []
+
+  graph.nodes().forEach(node => {
+    distances[node] = Infinity
+    visited[node] = false
+    path[node] = null
+  })
+
+  distances[startNode] = 0
+
+  queue.push({ node: startNode, distance: 0, dir: undefined, steps: 0 })
+
+  while (queue.length) {
+    queue.sort((a, b) => a.distance - b.distance)
+    const current = queue.shift()
+  }
+}
 
 function solution1(input) {
-  const formatted = formatInput(input)
+  const grid = formatInput(input)
+  const graph = createGraph()
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      const key = `${row},${col}`
+      graph.addNode(key)
+    }
+  }
+
+  graph.nodes().forEach(node => {
+    const [row, col] = node.split(',').map(Number)
+
+    const neighbors = [
+      [row - 1, col, 'N'],
+      [row + 1, col, 'S'],
+      [row, col - 1, 'W'],
+      [row, col + 1, 'E'],
+    ].filter(([r, c]) => grid?.[r]?.[c] !== undefined)
+
+    neighbors.forEach(([r, c, dir]) => {
+      const key = `${r},${c}`
+      const weight = Number(grid[r][c])
+
+      graph.addEdge(node, key, weight, dir)
+    })
+  })
+
+  graph.inspect()
+
+  return grid
 }
 
 function solution2(input) {}
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-17/index.test.js
+++ src/day-17/index.test.js
@@ -1,9 +1,23 @@
 const { solution1, solution2 } = require('./')
 
-const input = `input data for day 17`
+const input = `
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(102)
 })
 
 test('solution2', () => {
