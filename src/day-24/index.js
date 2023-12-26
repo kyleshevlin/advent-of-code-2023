d6fd585ae3f116d31225e39307cd6b5d2d5edcc9
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-25 17:19:30 -0800

    day 24 pt 1 solution

--- src/day-24/index.js
+++ src/day-24/index.js
@@ -1,16 +1,61 @@
-const { getInput } = require('../utils')
+const { getInput, getLineIntersection } = require('../utils')
 
 const data = getInput(__dirname)
 
-const formatInput = input => input
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .map(line => line.replace(' @', ',').split(', ').map(Number))
 
-function solution1(input) {
-  const formatted = formatInput(input)
+function getIntersections(items) {
+  const results = []
+
+  for (let i = 0; i < items.length; i++) {
+    for (let j = i + 1; j < items.length; j++) {
+      const [x1, y1, , vx1, vy1] = items[i]
+      const [x2, y2, , vx2, vy2] = items[j]
+
+      const crossProduct = vx1 * vy2 - vx2 * vy1
+
+      if (crossProduct === 0) continue
+
+      const dx = x2 - x1
+      const dy = y2 - y1
+
+      const t1 = (vy2 * dx - vx2 * dy) / crossProduct
+      const t2 = (vy1 * dx - vx1 * dy) / crossProduct
+
+      if (t1 < 0 || t2 < 0) continue
+
+      const intersectionX = x1 + t1 * vx1
+      const intersectionY = y1 + t1 * vy1
+
+      results.push({ x: intersectionX, y: intersectionY })
+    }
+  }
+
+  return results
 }
+
+function solution1(input, min, max) {
+  const stones = formatInput(input)
+  const intersections = getIntersections(stones)
+  const withinRange = intersections.filter(
+    ({ x, y }) => x > min && x < max && y > min && y < max
+  )
+
+  return withinRange.length
+}
+
+const solution1Min = 200_000_000_000_000
+const solution1Max = 400_000_000_000_000
+
+// console.log(solution1(data, solution1Min, solution1Max)) // 26611
 
 function solution2(input) {}
 
 module.exports = {
   solution1,
-  solution2
+  solution2,
 }
--- src/day-24/index.test.js
+++ src/day-24/index.test.js
@@ -1,11 +1,17 @@
 const { solution1, solution2 } = require('./')
 
-const input = `input data for day 24`
+const input = `
+19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input, 7, 27)).toEqual(2)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(47)
 })
